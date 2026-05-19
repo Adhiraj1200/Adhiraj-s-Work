037fad09a858542ca55051e3bfea94f3d2ebf586
Adhiraj1200 <25adhirajbhushan.acc@lwetb.ie>
2026-05-19 15:07:30 +0100

    Add files via upload

---
+++ RevWrks5/Summer Revision 5 Q1.py
@@ -0,0 +1,17 @@
+string = input(str("Enter a string:"))
+print(string)
+for word in string:
+    if word == "a":
+        print("a")
+        
+    if word == "e":
+        print("e")
+        
+    if word == "i":
+        print("i")
+        
+    if word == "o":
+        print("o")
+        
+    if word == "u":
+        print("u")    
---
+++ Revision-1/Revision Q1.py
@@ -0,0 +1,6 @@
+word = str(input("Enter a word:"))
+print(len(word))
+if len(word) < 10:
+    print("Be more imaginative")
+for x in word:
+    print(x)
