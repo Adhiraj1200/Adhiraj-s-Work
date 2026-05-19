651025f9c0442768b5f8ab74b5b6d5304c9b286b
Adhiraj1200 <25adhirajbhushan.acc@lwetb.ie>
2026-05-19 15:41:35 +0100

    Add files via upload

---
+++ Revision Q3.py
@@ -0,0 +1,23 @@
+word = str(input("Enter a word:"))
+print(len(word))
+if len(word) < 7:
+    print(":(")
+else:
+    print(":)")
+print(word[0])
+print(word[-1])
+letter = str(input("Enter a letter:"))    
+if letter == word:
+  print("f")
+    
+
+
+
+
+
+
+
+
+
+
+
